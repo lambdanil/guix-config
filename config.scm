9a8b6d1fbee8a9ca932969609120bc399f0beb93
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-03-24 22:27:24 +0100

    Add files via upload

---
+++ config.scm
@@ -0,0 +1,75 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+
+(use-modules (gnu)
+	     (nongnu packages linux)
+	     (nongnu system linux-initrd)
+             (gnu services virtualization)
+             (gnu packages fonts)
+             (guix packages))
+(use-service-modules desktop networking ssh xorg)
+
+(define %my-services
+  (modify-services %desktop-services
+             (guix-service-type config => (guix-configuration
+               (inherit config)
+               (substitute-urls
+                (append (list "https://substitutes.nonguix.org")
+                  %default-substitute-urls))
+               (authorized-keys
+                (append (list (local-file "./signing-key.pub"))
+                  %default-authorized-guix-keys)))))
+  )
+
+(define %my-packages
+  '("ungoogled-chromium"
+  ))
+
+(operating-system
+  (locale "cs_CZ.utf8")
+  (timezone "Europe/Prague")
+  (keyboard-layout (keyboard-layout "cz"))
+  (host-name "eternal")
+  (kernel linux)
+  (initrd microcode-initrd)
+  (firmware (list linux-firmware))
+  (users (cons* (user-account
+                  (name "jan")
+                  (comment "Jan Novotný")
+                  (group "users")
+                  (home-directory "/home/jan")
+                  (supplementary-groups
+                    '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+  (packages
+    (append
+      (list (specification->package "nss-certs"))
+      (list (specification->package "font-liberation"))
+      %base-packages
+      ))
+  (services
+    (append
+      (list (service gnome-desktop-service-type)
+            (set-xorg-configuration
+              (xorg-configuration
+                (keyboard-layout keyboard-layout))))
+      (list (service libvirt-service-type
+         (libvirt-configuration
+          (unix-sock-group "libvirt")
+          (tls-port "16555"))))
+      %my-services))
+  (bootloader
+    (bootloader-configuration
+      (bootloader grub-bootloader)
+      (targets '("/dev/sda"))
+      (keyboard-layout keyboard-layout)))
+  (swap-devices
+    (list (uuid "334f93d2-dc62-4529-8491-2d156f1d9ed5")))
+  (file-systems
+    (cons* (file-system
+             (mount-point "/")
+             (device
+               (uuid "5c9e25d1-058b-4a1d-a840-97d652af3862"
+                     'ext4))
+             (type "ext4"))
+           %base-file-systems)))
